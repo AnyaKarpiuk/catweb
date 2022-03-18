19ebb836e9fae3c9800f9dfb0ff1568983415ade
AnyaKarpiuk <anna-19.1994@mail.ru>
2022-03-18 22:12:56 +0200

    Add menu

---
+++ menu.js
@@ -0,0 +1,9 @@
+// change width of menu element to 100%
+function openMenu() {
+  document.getElementById("menu").style.width = "100%";
+}
+
+// change width of menu element to 0%
+function closeMenu() {
+  document.getElementById("menu").style.width = "0%";
+}
